97e188bd9276b87c941439911c2b08503c8df97f
carcigenicate <slomifier@gmail.com>
2017-08-01 21:17:34 -0700

    Added TODOs

--- src/snake/snake.clj
+++ src/snake/snake.clj
@@ -6,6 +6,8 @@
 (defn new-snake [starting-position]
   (->Snake [starting-position] [0 0]))
 
+; TODO: Food added at the head position causes an internal conflict.
+; TODO: Ignore where the food was eaten, add to tail by projecting the end of the tail's trajectory.
 (defn feed-at [snake food-position]
   (update snake :body
           #(conj % food-position)))
@@ -30,6 +32,9 @@
   (let [{body :body} snake]
     (not= (count body)
           (count (set body)))))
+
+; TODO: Allows the snake to double-back over itself.
+; TODO: Make sure the snake can't go up if it's going down, or left if it's going right etc.
 
 (defn move-up [snake]
   (change-direction snake [0 -1]))
